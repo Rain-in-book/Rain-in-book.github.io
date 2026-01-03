4377b7b477b0bfb396131d30f98949b272542db0
Rain-in-book <Starfm443@hotmail.com>
2026-01-03 14:11:14 +0800

    Add files via upload

---
+++ resource.js
@@ -0,0 +1,62 @@
+var resources = [
+    {
+        id: 1,
+        name: "Windows 2000 Server with SP4 简体中文版_ZRMSSEL_CN.iso",
+        size: "428.24MB",
+        link: "https://www.123684.com/s/zG2wjv-tTo63",
+    },
+    {
+        id: 2,
+        name: "IBM OS/2 2.1 简体中文版",
+        size: "39.38MB",
+        link: "https://www.123684.com/s/zG2wjv-LTo63",
+    },
+    {
+        id: 3,
+        name: "IBM OS/2 Warp 3.0 简体中文版",
+        size: "75.51MB",
+        link: "https://www.123684.com/s/zG2wjv-5To63",
+    },
+    {
+        id: 4,
+        name: "IBM OS/2 Warp 4.0 简体中文版",
+        size: "401.94MB",
+        link: "https://www.123684.com/s/zG2wjv-6To63",
+    },
+    {
+        id: 5,
+        name: "IBM OS/2 Warp 4.52 英语版",
+        size: "526.71MB",
+        link: "https://www.123684.com/s/zG2wjv-yTo63",
+    },
+    {
+        id: 6,
+        name: "日本《音·辞典™》VOL.2",
+        size: "893.28MB",
+        link: "https://www.123684.com/s/zG2wjv-VTo63",
+    },
+    {
+        id: 7,
+        name: "Windows NT Workstation 4.0（大中華版）.iso",
+        size: "548.22MB",
+        link: "https://www.123684.com/s/zG2wjv-jTo63",
+    },
+    {
+        id: 8,
+        name: "Windows直装小电脑 合集",
+        size: "14.42GB",
+        link: "https://www.123684.com/s/zG2wjv-Tvo63",
+    },
+    {
+        id: 9,
+        name: "98SIX.GHO",
+        size: "127.67MB",
+        link: "https://www.123684.com/s/zG2wjv-vvo63",
+    },
+    {
+        id: 10,
+        name: "天童爱丽丝 表情",
+        size: "26.09MB",
+        link: "https://www.123684.com/s/zG2wjv-dvo63",
+    }                          
+];
